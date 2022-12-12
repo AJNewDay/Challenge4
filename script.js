8de93eae075f6994a45e2fe3277a2db6807b9bd1
Aaron Jordan <aabjordan88@gmail.com>
2022-12-12 18:17:42 -0500

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-// Selects element by class
-//var timeEl = document.querySelector(".time");
+const beginButton = document.getElementById('begin-btn');
+const submitbtn = document.getElementById('submit-btn');
+let questionContainerElement = document.getElementById('question-container');
+let shuffledQuestions, currentQuestionIndex
+const questionElement = document.getElementById('question');
+const choicesButtonElement = document.getElementById('choices');
+beginButton.addEventListener('click', startQuiz);
+submitbtn.addEventListener('click', () => {
+    currentQuestionIndex++
 
-// Selects element by id
-//var mainEl = document.getElementById("main");
+    setNextQuestion()
+})
 
 
 function startQuiz() {
@@ -17,71 +24,126 @@ function startQuiz() {
             }
         }
     }, 1000);
-    document.getElementById("question-prompt").innerHTML;
-    // hide some stuff now
-    showQuestion()
-
-}
-function showQuestion() {
-    var promptContainer = document.getElementById('prompt-holder')
-    var choiceContainer = document.getElementById('choice-holder')
-    console.log(choiceContainer)
-    for (let i = 0; i < questions[currentIndex].choices.length; i++) {
-        console.log(i)
-        console.log(questions[currentIndex].choices[i])
-        var button = document.createElement('button')
-        //how am I going to put it on the screen? h1? button? div, and then put my buttons in there?
-        button.innerHTML = questions[currentIndex].choices[i]
-        choiceContainer.appendChild(button)
-        button.addEventListener('click', checkIfCorrectOrWrong)
-
-    }
-    promptContainer.innerHTML = questions.prompt;
-    var questioncontainer = document.createElement("h2");
-    questioncontainer.innerHTML = questions.prompt[0];
-    promptContainer.appendChild(questioncontainer);
-    console.log(questioncontainer);
+    beginButton.classList.add('hide')
+    shuffledQuestions = question.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
-var questions = [{
-    prompt: "Which example shows proper camel case?",
-    choices: ["Camelcase", "camelcaSe", "camelCase"],
-    answer: "camelCase",
-}];
-var currentIndex = 0
 
-function checkIfCorrectOrWrong(event) {
-    event.preventDefault()
-    console.log(event.target.innerHTML)
-    if (event.target.innerHTML === questions[currentIndex].answer) {
-        console.log()
-    } else {
-        console.log()
-    }
 
-    //maybe check if you're done with the quiz, if you are
-    if ("I'm done") {
-        endQuiz()
-    } else {
-        currentIndex++
-        showQuestion()
-    }
+function setNextQuestion() {
+    quizReset()
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
-function showQuestion() {
-    var choiceContainer = document.getElementById('choice-holder')
-    console.log(choiceContainer)
-    for (let i = 0; i < questions[currentIndex].choices.length; i++) {
-        console.log(i)
-        console.log(questions[currentIndex].choices[i])
-        var button = document.createElement('button')
-        //how am I going to put it on the screen? h1? button? div, and then put my buttons in there?
-        button.innerHTML = questions[currentIndex].choices[i]
-        choiceContainer.appendChild(button)
-        button.addEventListener('click', checkIfCorrectOrWrong)
+function showQuestion(question) {
 
+    questionElement.innerText = question.question;
+    question.choices.forEach(choices => {
+        const button = document.createElement('button');
+        button.innerText = choices.text;
+        button.classList.add('btn');
+        if (choices.correct) {
+            button.dataset.correct = choices.correct
+        }
+        button.addEventListener('click', submitAnswer);
+        choicesButtonElement.appendChild(button);
+    });
+
+
+}
+
+function quizReset() {
+
+    /* submitbtn.classList.add('hide'); */
+    while (choicesButtonElement.firstChild) {
+        choicesButtonElement.removeChild(choicesButtonElement.firstChild);
+    }
+
+}
+
+function submitAnswer(e) {
+
+    const chosenAnswer = e.target
+    const correct = chosenAnswer.dataset.correct
+    answerStatus(document.body, correct);
+    Array.from(choicesButtonElement.children).forEach(button => {
+        chosenAnswer(button, button.dataset.correct);
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1);
+
+
+}
+
+function answerStatus(element, correct) {
+
+    if (correct) {
+        correct();
+    }
+    else {
+        incorrect();
     }
 }
 
 
-// Loop through checked options to store in array
+function incorrect() {
+
+
+}
+
+function correct() {
+
+
+}
+
+
+
+const question = [
+
+    {
+        question: 'Which example displays proper camel case?',
+        choices: [
+            { text: 'Camelcase', correct: false },
+            { text: 'camelcaSe', correct: false },
+            { text: 'camelCase', correct: true }
+        ]
+    },
+
+    {
+        question: 'What is considered to be the "building-blocks" of webpages?',
+        choices: [
+            { text: 'HTML', correct: true },
+            { text: 'CSS', correct: false },
+            { text: 'JavaScript', correct: false }
+        ]
+    },
+
+    {
+        question: 'What allows us to add style to the elements of an HTML page?',
+        choices: [
+            { text: 'Java', correct: false },
+            { text: 'JavaScript', correct: false },
+            { text: 'CSS', correct: true }
+        ]
+    },
+
+    {
+        question: 'What allows us to create functions that make webpages interactive?',
+        choices: [
+            { text: 'HTML', correct: false },
+            { text: 'JavaScript', correct: true },
+            { text: 'CSS', correct: false }
+        ]
+    },
+
+    {
+        question: 'A script.js link should be placed where in the HTML page?',
+        choices: [
+            { text: 'Top', correct: false },
+            { text: 'Middle', correct: false },
+            { text: 'Bottom', correct: true }
+        ]
+    }
+]
